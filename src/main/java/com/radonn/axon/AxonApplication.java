8f9bdec977f7a0f138f440c8fb8e264348158346
nathan <nathan.dorny.etu>
2023-08-25 04:05:13 +0200

    modèle rangé

--- src/main/java/com/radonn/axon/AxonApplication.java
+++ src/main/java/com/radonn/axon/AxonApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import com.radonn.axon.config.Consts;
+import com.radonn.axon.discord.DiscordBotManager;
 import com.radonn.axon.wowApi.controllers.BlizzardApiController;
 import com.radonn.axon.wowApi.models.guild.Guild;
-import com.radonn.axon.wowApi.services.DiscordBotManager;
 
 import org.slf4j.Logger;
 
@@ -20,19 +20,14 @@ public class AxonApplication {
     public static Guild GuildCtrl;
 
     public static void main(String[] args) {
-        try {
-            ApplicationContext context = SpringApplication.run(AxonApplication.class, args);
+        ApplicationContext context = SpringApplication.run(AxonApplication.class, args);
         
-            AxonApplication.BnetCtrl = context.getBean(BlizzardApiController.class);
-            AxonApplication.GuildCtrl = BnetCtrl.getGuild("hyjal", "gardiens-éternels");
+        AxonApplication.BnetCtrl = context.getBean(BlizzardApiController.class);
+        AxonApplication.GuildCtrl = BnetCtrl.getGuild("hyjal", "gardiens-éternels");
             
-            String tok = context.getBean(Consts.class).getDiscordBotToken();
+        String tok = context.getBean(Consts.class).getDiscordBotToken();
 
-            context.getBean(DiscordBotManager.class).init();
-            System.out.println("Bot started !");;
-        } catch (Exception e) {
-            logger.error("Une erreur s'est produite lors de l'exécution de l'application", e);
-        }
-
+        context.getBean(DiscordBotManager.class).init();
+        System.out.println("Bot started !");
     }
 }
---
+++ src/main/java/com/radonn/axon/discord/controllers/ButtonCommand.java
@@ -0,0 +1,19 @@
+package com.radonn.axon.discord.controllers;
+
+import com.radonn.axon.discord.Builder;
+
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+
+public class ButtonCommand {
+
+    public static void lgeMenu(ButtonInteractionEvent event) {
+        if (event.getComponentId().equals("lge_menu_entretien")) event.getJDA().getGatewayPool().submit((Runnable) () -> {
+            event.replyModal(Builder.Modals.modEntretien(event.getUser())).queue();
+        });
+        if (event.getComponentId().equals("lge_menu_invite")) event.getJDA().getGatewayPool().submit((Runnable) () -> {
+            System.out.println("test");
+            event.replyModal(Builder.Modals.modInvite(event.getUser())).queue();
+        });
+    }
+    
+}
